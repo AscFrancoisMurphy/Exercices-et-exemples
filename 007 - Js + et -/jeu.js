d5d974f2600b24c4a5494fa00e68693c9d30b285
François Murphy <acs.francois.murphy@gmail.com>
2018-01-26 12:07:24 +0100

    Ajout de deux dossiers d'axercice, prépare un dossier pour l'exercice du retro game

--- 007 - Js + et -/jeu.js
+++ 007 - Js + et -/jeu.js
@@ -4,54 +4,69 @@ var randomNumber;
 var playerNumber;
 var maxValue = 100;
 var tries = 1;
-var response = false;
 
+//// Elements du DOM
 
+var playerField = document.getElementsByName("champJoueur");
+var textField1 = document.getElementById("retourTextuel1");
+var textField2 = document.getElementById("retourTextuel2");
+var submit = document.getElementById("submit");
 
 // Création des fonctions
 
-//Cette fonction génére un chiffre aléatoire
-//max : valeur max que peux prendre le chiffre aléatoire
-
 function setRandomNumber(max) {
-    return Math.floor(Math.random()*Math.floor(max)+1);
+    return Math.floor(Math.random() * Math.floor(max) + 1);
 };
 
-//// Initialiser la partie
+function sendValue() {
+    console.log(playerField.value);
+    return playerField.value;
 
-// Définir la valeur max
+};
 
-maxValue = Math.floor(prompt("Entrer la valeur max", 100));
+function initialise() {
 
-// Tirer un nombre
-
-randomNumber = setRandomNumber(maxValue);
-
-//// Lancer la partie
-
-console.log(randomNumber, maxValue);
-
-for( tries; tries<50; tries++ ) {
-
-    playerNumber = Math.floor(prompt("Faites votre proposition !", 100));
-
-    if(playerNumber>randomNumber) {
-        alert("trop grand!");
-    } else if(playerNumber<randomNumber) {
-        alert("trop petit!");
-    } else {
-        alert("Gagné !!");
-        break;
+    function setRandomNumber(max) {
+        return Math.floor(Math.random() * Math.floor(max) + 1);
     };
-    console.log(tries);    
-}
+
+    tries = 1;
+    maxValue = 100;
+    randomNumber = setRandomNumber(maxValue);
+    console.log(randomNumber, maxValue);
+};
+
+function jouer() {
+    initialise();
+    test();
+
+    function victoire() {
+        textField1.innerHTML = "Gagné !!";
+        textField2.innerHTML = "Gagné !!";
+
+    }
+
+    function test() {
+
+        function askANumber() {
+            textField1.innerHTML = "Quel est votre choix ?";
+            //à faire : Demander la valeur
+        }
+
+        function compare(playerNumber, randomNumber) {
+            if (playerNumber > randomNumber) {
+                textField2.innerHTML = "Trop grand!";
+            } else if (playerNumber < randomNumber) {
+                textField2.innerHTML = "Trop petit!";
+            } else {
+                victoire;
+            };
+        }
+    }
+
+};
 
 
+submit.addEventListener("click", sendValue);
 
-
-
-
-
-
-
-
+console.log(tries);
