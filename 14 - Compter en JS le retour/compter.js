aad433036412eb44362a5927006ed92d7343f031
François Murphy <acs.francois.murphy@gmail.com>
2018-01-29 10:08:31 +0100

    amélioration de l'exemple 14

--- 14 - Compter en JS le retour/compter.js
+++ 14 - Compter en JS le retour/compter.js
@@ -1,13 +1,15 @@
 console.log("slt");
 
-function compter(min, max, inter) {
+function compter(min, max, inter, mot) {
     for (i = min; i <= max; i = i + inter) {
         console.log(i);
     }
     console.log("Je sais compter de " + inter + " en " + inter);
     console.log("à partir de " + min + " et jusqu\'à " + max + " !!!");
+    console.log("et je pense au mot : " + mot);
+    
 }
 
-compter(1, 12, 3);
-compter(1, 20, 5);
-compter(501, 503, 1);
+compter(1, 12, 3, "nictalope");
+compter(1, 20, 5, "mayonnaise");
+compter(501, 503, 1, "alambic");
